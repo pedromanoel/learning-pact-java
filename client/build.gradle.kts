eebdf71e7dfb4ad6fafcb52925bfd92c7ce0e880
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2020-04-03 16:36:18 -0300

    Add pact broker

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.71"
+    id("au.com.dius.pact") version "4.0.8"
 }
 
 repositories {
@@ -12,6 +13,13 @@ dependencies {
 
     testImplementation("au.com.dius:pact-jvm-consumer-junit5:4.0.8")
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.1")
+}
+
+pact {
+    publish {
+        pactDirectory = "${project.buildDir}/pacts"
+        pactBrokerUrl = "http://localhost:9292"
+    }
 }
 
 tasks.withType<Test> {
